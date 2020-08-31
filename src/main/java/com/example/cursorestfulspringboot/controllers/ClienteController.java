8a5fe98f3cf0ce817de172a2977182cdade945aa
Koji <koji-yamada@hotmail.com>
2020-08-31 20:36:38 -0300

    Adicionou classe clientes

--- src/main/java/com/example/cursorestfulspringboot/controllers/ClienteController.java
+++ src/main/java/com/example/cursorestfulspringboot/controllers/ClienteController.java
@@ -1,5 +1,10 @@
 package com.example.cursorestfulspringboot.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,13 +12,49 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ClienteController {
     
-    @GetMapping("/clientes")
-    public String getCliente(){
-        return "Vai retornar todos os clientes da base";
+    public List<Cliente> clientes;
+
+    @PostConstruct
+    public void init(){
+        Cliente c1 = new Cliente();
+        c1.id = 1;
+        c1.nome = "Jose";
+        c1.endereco = "Rua X, 99";
+        c1.saldo = 202.00;
+
+        Cliente c2 = new Cliente();
+        c2.id = 2;
+        c2.nome = "Maria";
+        c2.endereco = "Rua Y, 59";
+        c2.saldo = 444.00;
+
+        Cliente c3 = new Cliente();
+        c3.id = 3;
+        c3.nome = "Fernanda";
+        c3.endereco = "Rua Z, 43";
+        c3.saldo = 569.00;
+
+        clientes = Arrays.asList(c1, c2, c3);
     }
 
-    @GetMapping("/clientes/{codigo}")
-    public String getClienteByCodigo(@PathVariable final int codigo) {
-        return "Vai retornar o cliente de codigo: " + codigo;
+
+
+    @GetMapping("/clientes")
+    public List<Cliente> getClientes(){
+        return clientes;
+    }
+
+    @GetMapping("/clientes/{id}")
+    public Cliente getClienteByCodigo(@PathVariable int id) {
+        
+        Cliente cliente = null;
+        
+        for(Cliente aux : clientes){
+            if(aux.id == id){
+                cliente = aux;
+                break;
+            }
+        }
+        return cliente;
     }
 }
